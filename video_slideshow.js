2f3ec310a7c7db6715a62a3a14e0b38416ee6043
realendie <enderproplays54@gmail.com>
2025-03-23 22:32:44 -0400

    Fixed bug where video would initially not show

--- video_slideshow.js
+++ video_slideshow.js
@@ -2,8 +2,11 @@ const videos = [
     "https://www.youtube.com/embed/w_GjBGwlPCU?si=COU1MZARLfwda7mC",
     "https://www.youtube.com/embed/GxzfC56Hl30",
 ];
+
 let currentVideo = 0;
 const videoFrame = document.getElementById("videoFrame");
+
+videoFrame.src = videos[currentVideo];
 
 document.getElementById("nextBtn").addEventListener("click", () => {
     currentVideo = (currentVideo + 1) % videos.length;
